3d5ac40dd3d800d136fcc26f5f6c5b8b89920914
Mike Falk <falk478@yahoo.com>
2021-06-21 11:34:03 -0700

    Add project files.

---
+++ HelloWorldBasicCSharpReview/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace HelloWorldBasicCSharpReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //VariableExamples();
+
+            /*
+            // These are common types in C#.... 
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mike";
+            bool isWorking = true;
+            var newNumber = 13;
+
+            number = 3;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(newNumber);
+            */
+
+            // A const cannot be changed after it is declared 
+            // const float Pi = 3.14f;
+            
+
+            // This is an example of placeholders using format strings...
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            
+        }
+    }
+}
---
+++ HelloWorldBasicCSharpReview/Variables.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldBasicCSharpReview
+{
+    class Variables
+    {
+        public void VariableExamples()
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mike";
+            bool isWorking = true;
+            var newNumber = 13;
+
+            number = 3;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(newNumber);
+
+        }
+    }
+}
